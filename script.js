8bc44695f8aa3cdbc9d9cab8f46c27b304ce1a03
Brosa <brunoarosa1997@gmail.com>
2023-02-11 12:52:48 -0500

    Updated global variables

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Assignment code here
+// Global Variables
 let letters = ["abcdefghijklmnopqrstuvwxyz"];
 let arrayLetters = [...letters[0]];
 let upperLetters = letters[0].toUpperCase();
 let arrayUpperletters = [...upperLetters];
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let specialC = ["!", "@", "#", "$", "%", "^", "&", "*"];
-let arrayOfoptions = [];
-let randomPwd = [];
 
 function generatePassword() {
+  let randomPwd = [];
+  let arrayOfoptions = [];
 
   //LENGTH CONDITIONS
   let pwdlength = parseInt(prompt("Please indicate desired length of password"), 10);
@@ -74,17 +74,19 @@ function generatePassword() {
   }
 
   // Ensures user criteries are met
+  // To add some more randomness hit this aray with another for loop
   randomPwd.push(randomU, randomL, randomN, randomS)
 
   // Converts the Array to string and removes commas
   let randomPwdS = randomPwd.toString()
   let randomPwdClean = randomPwdS.replace(/,/g, "")
 
+  console.log(randomPwd)
+
   return randomPwdClean
 }
 
-// To add some more randomness to it I should 
-// TODO: need to reset the randomPwd array to empty
+// TODO: need to reset the arrayOfoptions array to empty
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
